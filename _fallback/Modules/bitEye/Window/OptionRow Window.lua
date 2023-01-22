5262da0623fd641f35a0edf268d08baa7fb402b7
EngineMachiner <mfpc98@gmail.com>
2023-01-21 20:36:15 -0500

    Update OptionRow Window.lua

--- _fallback/Modules/bitEye/Window/OptionRow Window.lua
+++ _fallback/Modules/bitEye/Window/OptionRow Window.lua
@@ -168,7 +168,6 @@ return Def.ActorFrame{
 
 					if OptionRow.canShow and isVisible then
 						self:stoptweening():linear(0.25):diffusealpha(1)
-						Update( OptionRow.AFT ) 
 					end
 	
 					if not isVisible then close(self) end
