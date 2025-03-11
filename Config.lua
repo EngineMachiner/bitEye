6760fbd47c6b61ab0fd3ee13d9d41135a958f14a
EngineMachiner <mfpc98@gmail.com>
2025-03-11 16:18:30 -0300

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -6,15 +6,15 @@ bitEye.Config = {
     AnimationSpeed = 1, -- Sprite animation and tweening.
 
 
-    -- The EditNoteField preview is centered at { x = 0, y = 0 }
+    -- The EditNoteField preview is centered when the vector is zero.
 
     -- In newest versions it follows the edit notefield position.
 
-    EditNoteField = {  Pos = Vector { x = 425, y = 0 }  },
+    EditNoteField = { Pos = Vector { x = 425, y = 0 } },
     
 
-    -- The OptionRow preview is at the center top when { x = 0, y = 0 }
+    -- The OptionRow preview is at the center top when the vector is zero.
 
-    OptionRow = {  Pos = Vector { x = 0, y = 0 },   ZoomIn = 0.25 }
+    OptionRow = { Pos = Vector(),   ZoomIn = 0.25 }
     
 }
