4e84aa714d3715f5abe76513b3110acb6c633db0
EngineMachiner <mfpc98@gmail.com>
2025-07-02 10:21:52 -0500

    Add bash script to setup things easily.

---
+++ bitEye.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -e
+
+FALLBACK="Appearance/Themes/_fallback";         SCRIPTS="$FALLBACK/Scripts"
+
+MODULES="Modules";          BGANIMATIONS="$FALLBACK/BGAnimations"
+
+
+# Check modules folder.
+
+if [ -d "$FALLBACK/$MODULES" ]; then MODULES="$FALLBACK/$MODULES"; fi
+
+
+# Clone repository.
+
+REPOSITORY="https://github.com/EngineMachiner/bitEye.git"
+
+#git clone "$REPOSITORY" "$MODULES/bitEye"
+
+
+# Add to init script.
+
+echo "Checking initialization script...";           TAPLUA="$SCRIPTS/tapLua.lua"
+
+echo "LoadModule(\"bitEye/bitEye.lua\")" | { grep -xFv -f "$TAPLUA" >> "$TAPLUA" || true; }
+
+echo "Done."
+
+
+# Open editor to add actors.
+
+cat << EOF
+
+Add the actors to each screen layer so they are loaded:
+
+bitEye.actor("OptionRow") -- ScreenMiniMenuBackgroundChange
+bitEye.actor("EditNoteField") -- ScreenEdit
+
+EOF
+
+read -p "Nano will open. Press any key to continue."
+
+sudo nano "$BGANIMATIONS/ScreenMiniMenuBackgroundChange overlay.lua"
+sudo nano "$BGANIMATIONS/ScreenEdit overlay.lua"
